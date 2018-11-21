59704e13d06f77d434b1272b4f6b66e43e894dbd
attepee <atte.pekkarinen@outlook.com>
2018-11-21 16:21:12 +0200

    Added files, started making css and page bases

---
+++ profile.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Profile | Project Manager</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div id="header">
+            <div id=logo>
+                <p>Project Manager</p>
+            </div>
+            <div id="navBar">
+                <ul>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Statistics</a></li>
+                    <li><a href="#">Project</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="content">
+
+        </div>
+    </body>
+</html>
