25045e952b0aa707e0d32fe8dfba5558bf7d4473
Atte Pekkarinen <35224480+attepee@users.noreply.github.com>
2019-03-13 15:15:11 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-    $db = new PDO('mysql:host=mysql.labranet.jamk.fi;dbname=L9406_1;charset=utf8', 'L9406', '');
+    $db = new PDO('mysql:host=;dbname=;charset=utf8', '', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
